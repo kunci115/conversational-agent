fe0a495e06df9649955f5aadc0345bfe6b16b309
kunci115 <rino.alpin@gmail.com>
2024-06-18 15:24:58 +0700

    product payment

---
+++ payment/process.py
@@ -0,0 +1,5 @@
+def checkout(total_price, product_list, token_payment):
+    try:
+        return True
+    except:
+        return False
---
+++ product/pizza.py
@@ -0,0 +1,37 @@
+def product_pizza():
+    pizza_detail = [
+                    {"id": "1",
+                    "name": "Meat Lovers",
+                    "ingredient": "Burger Sapi, Daging sapi, Daging cincang, sosis sapi, sosis ayam, keju mozarella",
+                    "price": 140909
+                    },
+                    {"id": "2",
+                    "name": "American Favourite",
+                    "ingredient": "Pepperoni, Beef topping, jamur, keju mozarella, onion, saus pizza spesial",
+                    "price": 140909
+                    },
+                    {"id": "3",
+                    "name": "Chicken Supreme",
+                    "ingredient": "Daging Ayam Asap, Chicken Stick, Jamur, Nanas, Paprika, dan Keju mozarella",
+                    "price": 140909
+                    },
+                    {"id": "4",
+                    "name": "Super Supreme",
+                    "ingredient": "Daging Sapi Asap, Daging ayam asap, Burger sapi, Jamur, nanas, paprika dan keju mozarella",
+                    "price": 140909
+                    },
+                    {"id": "5",
+                    "name": "Chicken Lovers",
+                    "ingredient": "Dengan sosis ayam, chicken luncheon, chicken thicks, dan gurihnya keju, mozarella keju, mozarella keju saus spesial",
+                    "price": 140909
+                    }
+                    
+                    ]
+    return pizza_detail
+
+
+def checkout(total_price, product_list, token_payment):
+    try:
+        return "Payment Success"
+    except:
+        return False
